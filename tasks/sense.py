e8221327b40505b30cd1f3a42587f8bf1069a7f9
Naveen Singh <naveen@physiz.com>
2017-06-20 20:16:42 +0530

    Data logging format changed

--- tasks/sense.py
+++ tasks/sense.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import uuid
 import base64
 import uuid
 import os
 import sys
 import json
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                os.path.pardir)))
 
 from datetime import datetime
 from dateutil.tz import tzlocal
@@ -13,29 +15,48 @@ from modules.sensor import getTempC, getHumidity
 from modules.camera import captureImage
 from db import bulkStore
 
+
 # Get the current date/time with the timezone.
 
 def loadConfig():
-	with open('./config/recipe.json') as data_file:
-	    data = json.load(data_file)
-	    return data
+    with open('./config/recipe.json') as data_file:
+        data = json.load(data_file)
+        return data
+
 
 config = loadConfig()
 
-deviceID = "0000ad19b01"
+deviceID = '0000ad19b01'
 pID = os.getpid()
 appID = 1
+
+
 # get a UUID - URL safe, Base64
+
 def get_a_uuid():
     r_uuid = base64.urlsafe_b64encode(uuid.uuid4().bytes)
     return r_uuid.replace('=', '')
 
+
 def capture():
-	temp = getTempC()
-	hum = getHumidity()
-	eventID = get_a_uuid()
-	imgName = config["PlantName"]+eventID
-	captureImage(imgName)
-	createdAt = str(datetime.now(tzlocal()))
-	eventData = [{"appID": appID, "pID":pID, "deviceID":deviceID, "eventID":eventID, "event":"capture-temp", "eventVal": temp, "createdAt":createdAt },{"appID": appID, "pID":pID, "deviceID":deviceID, "eventID":eventID, "event":"capture-hum", "eventVal":hum, "createdAt":createdAt },{"appID": appID, "pID":pID, "deviceID":deviceID, "eventID":eventID, "event":"capture-img", "path":imgName, "createdAt":createdAt }]
-	bulkStore(eventData)
+    temp = getTempC()
+    hum = getHumidity()
+    eventID = get_a_uuid()
+    imgName = config['PlantName'] + eventID
+    captureImage(imgName)
+    createdAt = str(datetime.now(tzlocal()))
+    eventData = [{
+        'appID': appID,
+        'pID': pID,
+        'deviceID': deviceID,
+        'eventID': eventID,
+        'temp': temp,
+        'hum': hum,
+        'img-path': imgName,
+        'createdAt': createdAt,
+        }]
+    bulkStore(eventData)
+
+
+
+			
